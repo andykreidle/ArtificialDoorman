cedd739d82295843390bf3d843986e8c714cf3de
andykreidle <andkreidle@gmail.com>
2015-12-01 09:58:46 -0800

    Update arrivalNote.sh
    sends notification to cell phone over email/SMS, send picture to your email

--- arrivalNote.sh
+++ arrivalNote.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-mutt -s "  A visitor has arrived!" PHONENUMBER@txt.att.net < arrival.txt
+while true
+do
+#read the ADC/piezo until it hits the threshold
+sudo python piezo.py
+#or run the passive infrared sensor(motion sensor)
+#sudo python monitor_camera.py
+#send a notification
+mutt -s "    Someone has arrived" PHONENUMBER@txt.att.net < arrival.txt
+#send the picture
+mutt -s " guest picture " YOURemail@gmail.com -a guest.jpg < nada.txt
+done
